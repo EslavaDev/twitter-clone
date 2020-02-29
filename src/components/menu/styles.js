0e0df22e51a11247f49ac10705e85ea99377be74
daniel alejandro eslava herrera <danielalejandroeslavaherrera@daniels-MacBook-Pro-2.local>
2020-02-29 15:00:35 -0500

    body grid

---
+++ src/components/menu/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const DivMenuLeftStyled = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    & > .ui.secondary.vertical.menu>.item {
+        color: white;
+        font-size: 20px;
+        i.icon {
+            margin: 0;
+            margin-right:10px;
+        }
+    }
+`
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,8 @@ import { useRouter } from 'next/router'
 import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import { getSearch } from '../store/layout/selectors'
-import Layout from '../components/layout';
+import Layout from '../components/layout/layout';
+import { Button } from 'semantic-ui-react'
 
 const Title = styled.div`
   font-size: 50px;
@@ -27,16 +28,18 @@ const Index = () => {
     const router = useRouter()
     const selector = useSelector(getSearch)
     const handleclick = () => {
-        console.log(selector);
+      router.pathname
+        console.log(router.pathname, router.route);
+        console.log(router.asPath);
         // router.push('/about')
     }
     const handleChange = date => {
       setDate(date);
     };
-return <Layout>
+return <Layout title={"Inicio"}>
   <Title>
     <h1>My page</h1>
-
+    <Button onClick={handleclick}>H!</Button>
     <div style={{ marginTop: 20 }}>
           Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
         </div>
