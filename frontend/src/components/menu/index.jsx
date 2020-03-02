476609b68d0a4d18e6059babc912ba92f200d444
Daniel Eslava <daniel.eslava@frubana.com>
2020-03-02 14:35:46 -0500

    refactor menu

--- frontend/src/components/menu/index.jsx
+++ frontend/src/components/menu/index.jsx
@@ -1,98 +1,42 @@
-import React, {useState} from 'react'
-import { useRouter } from 'next/router'
-import {  Menu, Icon } from 'semantic-ui-react'
-import { DivMenuLeftStyled } from './styles';
-import { useSelector, useDispatch } from 'react-redux';
-import { getActiveBar } from '../../store/layout/selectors';
-import { changeActiveBar } from '../../store/layout/actions';
-import { ButtonTwiiter } from '../button/styled';
-
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { Menu, Icon } from "semantic-ui-react";
+import { DivMenuLeftStyled } from "./styles";
+import { useSelector, useDispatch } from "react-redux";
+import { getActiveBar } from "../../store/layout/selectors";
+import { changeActiveBar } from "../../store/layout/actions";
+import { ButtonTwiiter } from "../button/styled";
+import { menuItem } from "../../helpers/constants";
 
 export const MenuLeft = () => {
-  const active = useSelector(getActiveBar)
-  const dispatch = useDispatch()
-    const router = useRouter()
-    const handleItemClick = async (e, { name, route }) =>{
-      try {
-        await router.push(route)
-        dispatch(changeActiveBar(name));
-      } catch (error) {
-        
-      }
-    }
+  const active = useSelector(getActiveBar);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const handleItemClick = async (e, { name, route }) => {
+    try {
+      await router.push(route);
+      dispatch(changeActiveBar(name));
+    } catch (error) {}
+  };
 
-return (
+  return (
     <DivMenuLeftStyled>
-    <Menu  secondary vertical>
-    <Menu.Item
-      name='home'
-      route="/home"
-      
-      active={active === 'home'}
-      onClick={handleItemClick}
-    >
-        <Icon name='home' />
-        Inicio
-    </Menu.Item>
-    <Menu.Item
-      name='explore'
-      route="/contact"
-      active={active === 'explore'}
-      onClick={handleItemClick}
-    >
-        <Icon name='hashtag' />
-        Explorar
-    </Menu.Item>
-    <Menu.Item
-      name='notification'
-      route="/about"
-      active={active === 'notification'}
-      onClick={handleItemClick}
->
-<Icon name='bell outline' />
-        Notificaciones
-    </Menu.Item>    
-    <Menu.Item
-      name='messages'
-      route="/home"
-      
-      active={active === 'messages'}
-      onClick={handleItemClick}
-    >
-        <Icon name='mail outline' />
-        Mensajes
-    </Menu.Item>
-    <Menu.Item
-      name='saved'
-      route="/contact"
-      active={active === 'saved'}
-      onClick={handleItemClick}
-    >
-        <Icon name='bookmark outline' />
-        Guardados
-    </Menu.Item>
-    <Menu.Item
-      name='lists'
-      route="/home"
-      active={active === 'lists'}
-      onClick={handleItemClick}
-    >
-        <Icon name='list alternate outline' />
-        Listas
-    </Menu.Item>
-    <Menu.Item
-      name='profile'
-      route="/home"
-      
-      active={active === 'profile'}
-      onClick={handleItemClick}
-    >
-        <Icon name='user outline' />
-        Perfil
-    </Menu.Item>
-    <ButtonTwiiter size="big" fluid >TWITTEAR</ButtonTwiiter>
-  </Menu>
-
+      <Menu secondary vertical>
+        {menuItem.map(menuItemData => (
+          <Menu.Item
+            onClick={handleItemClick}
+            active={active === menuItemData.name}
+            name={menuItemData.name}
+            route={menuItemData.route}
+          >
+            <Icon name={menuItemData.icon} />
+            {menuItemData.label}
+          </Menu.Item>
+        ))}
+        <ButtonTwiiter size="big" fluid>
+          TWITTEAR
+        </ButtonTwiiter>
+      </Menu>
     </DivMenuLeftStyled>
-)
-    }
+  );
+};
---
+++ frontend/src/helpers/constants.js
@@ -0,0 +1,47 @@
+export const menuItem = [
+  {
+    name: "home",
+    route: "/home",
+    icon: "home",
+    label: "Inicio"
+  },
+  {
+    name: "explore",
+    route: "/contact",
+    icon: "hashtag",
+    label: "Explorar"
+  },
+  {
+    name: "notification",
+    route: "/about",
+    icon: "bell outline",
+    label: "Notificaciones"
+  },
+  {
+    name: "message",
+    route: "/home",
+    icon: "mail outline",
+    label: "Mensajes"
+  },
+
+  {
+    name: "saved",
+    route: "/contact",
+    icon: "bookmark outline",
+    label: "Guardados"
+  },
+
+  {
+    name: "lists",
+    route: "/home",
+    icon: "list alternate outline",
+    label: 'Listas'
+  },
+
+  {
+    name: "profile",
+    route: "/home",
+    icon: "user outline",
+    label: 'Perfil'
+  }
+];
