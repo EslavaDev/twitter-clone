714931f30000ed89927f453f88759ed5c31e1835
Daniel Eslava <daniel.eslava@frubana.com>
2020-03-09 18:35:29 -0500

    fix 2

--- frontend/src/hooks/firebase.jsx
+++ frontend/src/hooks/firebase.jsx
@@ -1,32 +1,56 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import * as firebaseApp from "firebase"
-import firebaseKeys from './keys.json';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import * as firebaseApp from "firebase";
+import firebase from "@firebase/app";
+import "@firebase/database";
 
-const app = !firebaseApp.apps.length ? firebaseApp.initializeApp(firebaseKeys) : firebaseApp.app();
+import firebaseKeys from "./keys.json";
 
-  export const useDataBase = (url, action) => {
-    const dispatch = useDispatch();
+const app = !firebaseApp.apps.length
+  ? firebaseApp.initializeApp(firebaseKeys)
+  : firebaseApp.app();
 
-    useEffect(()=>{
-      if ( url === null) return;
-      let listeners = [];
-        const ref = app.database().ref(url);
-        listeners.push(
-          ref.on('value', snapshot => {
-            if(snapshot && snapshot.val){
-              dispatch(action(snapshot.val()))
-            }
-          })
-        )
-      return () => {
-        for (let unsub of listeners) {
-          unsub();
+export const useDataBase = (url, action) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (url === null) return;
+    let listeners = [];
+    const ref = app.database().ref(url);
+    listeners.push(
+      ref.on("value", snapshot => {
+        if (snapshot && snapshot.val) {
+          dispatch(action(snapshot.val()));
         }
-      };
-    }, [url, action])
+      })
+    );
+    return () => {
+      for (let unsub of listeners) {
+        unsub();
+      }
+    };
+  }, [url, action]);
+};
 
-    
-}
+const loadDB = () => {
+  try {
+    const config = {
+      apiKey: "AIzaSyCgQmJyPmf3fRHQMxi2WRqHFG7bzywCUkA",
+      authDomain: "merco-web.firebaseapp.com",
+      databaseURL: "https://merco-web.firebaseio.com",
+      projectId: "merco-web",
+      storageBucket: "merco-web.appspot.com",
+      messagingSenderId: "290035762727",
+      appId: "1:290035762727:web:f679aec93f76e0fc16f002"
+    };
+    firebase.initializeApp(config)
+    // firebase.database().ref().o
+  } catch (error) {
+    if(!/already exist/.test(error.message)){
+      console.error('Firebase initialization error', error.stack)
+    }
+  }
+  return firebase
+};
 
-  
+export default loadDB;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,33 +2,35 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Layout from "../components/layout/layout";
 import { BoxSend } from "../components/box-send";
-import { useDataBase } from "../hooks/firebase";
+import loadFirebase, { useDataBase } from "../hooks/firebase";
 import { fetchTweets } from "../store/tweets/actions";
 import { getTweetsData } from "../store/tweets/selectors";
 import { BoxRecive } from "../components/box-recive";
-
-const Home = ({tweetsSelector}) => {
-  // useDataBase("/tweets", fetchTweets);
-  console.log(tweetsSelector)
+const Home = ({ props }) => {
+  useDataBase("/tweets", fetchTweets);
+  const tweetsSelector = useSelector(getTweetsData)
+  console.log(props, tweetsSelector);
   return (
     <Layout title={"Inicio"}>
-
       <BoxSend>
-      <BoxRecive data={tweetsSelector} />
+        <BoxRecive data={tweetsSelector} />
       </BoxSend>
     </Layout>
   );
 };
 
-export async function getServerSideProps() {
-  // Fetch data from external API
-  await useDataBase("/tweets", fetchTweets);
-  const tweetsSelector = useSelector(getTweetsData);
-  console.log(tweetsSelector, 'test')
-
-  // Pass data to the page via props
-  return { props: { tweetsSelector } }
-}
-
+Home.getInitialProps = async (props) => {
+  const firebase = await loadFirebase();
+  const db = firebase.database();
+  const ref = db.ref('/tweets')
+      ref.once("value", snapshot => {
+        if (snapshot && snapshot.val) {
+          props.store.dispatch(fetchTweets(snapshot.val()))
+          return snapshot.val()
+        }
+      })
+    
+  return {data: 'none'};
+};
 
 export default Home;
